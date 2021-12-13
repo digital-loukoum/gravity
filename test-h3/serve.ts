03aa6a05383cb3575b41045265fd1796628af12a
lepzulnag <gindre.matthieu@gmail.com>
2021-12-13 15:53:50 +0100

    ✨ serving with h3 works

--- test-h3/serve.ts
+++ test-h3/serve.ts
@@ -1,9 +1,13 @@
 import { createServer } from "http"
 import { createApp } from "h3"
-import { gravity } from "@digitak/gravity/middleware"
+import { gravity } from "@digitak/gravity/library/middleware/index.js"
 import { services } from "./services"
+
+const PORT = 3000
 
 const app = createApp()
 app.use(gravity({ services }))
 
-createServer(app).listen(process.env.PORT || 3000)
+createServer(app).listen(PORT)
+
+console.log("Serving port", PORT, "...")
