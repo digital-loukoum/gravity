68b9613398fe9788b2e54fe7cb897e71cf65e86d
lepzulnag <gindre.matthieu@gmail.com>
2022-03-04 10:14:25 +0100

    add astro

---
+++ documentation/astro.config.mjs
@@ -0,0 +1,7 @@
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
+	renderers: ["@astrojs/renderer-react"],
+});
