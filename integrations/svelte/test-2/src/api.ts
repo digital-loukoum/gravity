2735d95c143fb405029ffe39f5ff192b8e4f7f02
lepzulnag <gindre.matthieu@gmail.com>
2022-04-22 15:16:29 +0200

    ⚰️ remove unused test folder

--- integrations/svelte/test-2/src/api.ts
+++
@@ -1,6 +0,0 @@
-import type { services } from "./services/index.js";
-import { defineApi } from "@digitak/gravity-svelte";
-
-export const { api, useApi } = defineApi<services>({
-	// additional options go there
-});
--- integrations/svelte/test-2/src/services/index.ts
+++
@@ -1,5 +0,0 @@
-import { math } from "./math/math.js";
-
-export const services = {
-	math,
-};
