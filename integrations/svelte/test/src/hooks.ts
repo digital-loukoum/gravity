b6adb93b139eeaf956aac59a7ebf2b9dfbbf47b1
lepzulnag <gindre.matthieu@gmail.com>
2022-04-24 17:05:24 +0200

    🔥 remove console log

--- integrations/svelte/test/src/hooks.ts
+++ integrations/svelte/test/src/hooks.ts
@@ -3,8 +3,6 @@ import { services } from './gravity/services';
 import type { Context } from './gravity/Context';
 import schema from './gravity/schema.json';
 
-console.log('schema', schema);
-
 export const handle = defineHandler<Context>({
 	services,
 	schema,
