51871a0d7f7965e094e46a327f0b1adc7cd9c257
lepzulnag <gindre.matthieu@gmail.com>
2022-05-02 17:38:28 +0200

    🐛 fix svelte-kit template handler

--- packages/gravity/templates/svelte-kit/src/hooks.ts
+++ packages/gravity/templates/svelte-kit/src/hooks.ts
@@ -1,9 +1,8 @@
 import { defineHandler } from "@digitak/gravity-svelte-kit/server";
 import { services } from "./services/index.js";
-import type { Context } from "./services/Context.js";
 import schema from "./schema.json";
 
-export const { handle } = defineHandler<Context>({
+export const { handle } = defineHandler({
 	apiPath: "/api",
 	services,
 	schema,
